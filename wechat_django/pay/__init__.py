6530de17867ba66968bca9057369f68e496f634b
bopo <ibopo@126.com>
2022-06-18 12:53:14 +0800

    update commit

--- wechat_django/pay/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-default_app_config = 'wechat_django.pay.apps.WeChatPayConfig'
--- wechat_django/pay/exceptions.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-
-class WeChatPayNotifyError(ValueError):
-    def __init__(self, msg, inner=None):
-        self.msg = msg
-        self.inner = inner
